fa326856a843a739b4da7e6efe37335965736cf6
Jayant Bangia <127044321+jayantb11@users.noreply.github.com>
2023-12-30 18:14:09 -0500

    Update word_function.sql

--- word_function.sql
+++ word_function.sql
@@ -1,48 +1,60 @@
--- create database work_function;
--- CREATE TABLE employees (
---     emp_no INT PRIMARY KEY AUTO_INCREMENT,
---     department VARCHAR(20),
---     salary INT
--- );
---  
--- INSERT INTO employees (department, salary) VALUES
--- ('engineering', 80000),
--- ('engineering', 69000),
--- ('engineering', 70000),
--- ('engineering', 103000),
--- ('engineering', 67000),
--- ('engineering', 89000),
--- ('engineering', 91000),
--- ('sales', 59000),
--- ('sales', 70000),
--- ('sales', 159000),
--- ('sales', 72000),
--- ('sales', 60000),
--- ('sales', 61000),
--- ('sales', 61000),
--- ('customer service', 38000),
--- ('customer service', 45000),
--- ('customer service', 61000),
--- ('customer service', 40000),
--- ('customer service', 31000),
--- ('customer service', 56000),
--- ('customer service', 55000);
+-- Create a database 'work_function'
+CREATE DATABASE work_function;
 
+-- Switch to the 'work_function' database
+USE work_function;
 
--- use work_function;
--- select department,AVG(salary) from employees group by 1
--- select emp_no,department,salary,AVG(salary) OVER(),MIN(salary) OVER(),MAX(salary)  OVER()  from employees 
--- select emp_no,department,salary,AVG(salary) OVER(partition by department) as dep_avg from employees 
--- select emp_no,department,salary,COUNT(*) over(partition by department) as dept_count from employees
--- select emp_no,department,salary,sum(salary) over() as total_salary,sum(salary) OVER(partition by department) as dep_salary 
--- from employees
--- select emp_no,department,salary,sum(salary) over(partition by department order by salary) from employees
--- select emp_no,department,salary,MIN(salary) over(partition by department order by salary DESC) from employees
--- select emp_no,department,salary,RANK() OVER(partition by department order by salary desc) as overall_salary_rank from employees
--- select emp_no,department,salary,row_number() OVER(partition by department order by salary desc) as overall_salary_rank from employees
--- select emp_no,department,salary,dense_rank() OVER(partition by department order by salary desc) as dense_salary from employees
--- use work_function;
--- select emp_no,department,salary,NTILE(4) OVER(ORDER BY salary desc) as salary_quartile,
--- NTILE(4) OVER(partition by department order BY salary desc) as dept_salary_quartile from employees
--- select emp_no,department,salary,first_value(emp_no)  OVER(partition by department ORDER BY salary desc) from employees
-select emp_no,department,salary,salary - LAG(salary) over ( partition by department order by salary desc) from employees
+-- Create a table 'employees' with columns 'emp_no', 'department', and 'salary'
+CREATE TABLE employees (
+    emp_no INT PRIMARY KEY AUTO_INCREMENT,
+    department VARCHAR(20),
+    salary INT
+);
+
+-- Insert records into the 'employees' table
+INSERT INTO employees (department, salary) VALUES
+('engineering', 80000),
+('engineering', 69000),
+...
+('customer service', 56000),
+('customer service', 55000);
+
+-- Switch to the 'work_function' database
+USE work_function;
+
+-- Select the average salary for each department
+SELECT department, AVG(salary) FROM employees GROUP BY 1;
+
+-- Select employee details with additional columns for overall average, minimum, and maximum salary
+SELECT emp_no, department, salary, AVG(salary) OVER(), MIN(salary) OVER(), MAX(salary) OVER() FROM employees;
+
+-- Select employee details with the average salary for each department
+SELECT emp_no, department, salary, AVG(salary) OVER(PARTITION BY department) AS dep_avg FROM employees;
+
+-- Select employee details with the count of employees in each department
+SELECT emp_no, department, salary, COUNT(*) OVER(PARTITION BY department) AS dept_count FROM employees;
+
+-- Select employee details with total and department-wise salary sum
+SELECT emp_no, department, salary, SUM(salary) OVER() AS total_salary, SUM(salary) OVER(PARTITION BY department) AS dep_salary FROM employees;
+
+-- Select employee details with cumulative sum of salary within each department
+SELECT emp_no, department, salary, SUM(salary) OVER(PARTITION BY department ORDER BY salary) FROM employees;
+
+-- Select employee details with minimum salary within each department in descending order
+SELECT emp_no, department, salary, MIN(salary) OVER(PARTITION BY department ORDER BY salary DESC) FROM employees;
+
+-- Select employee details with overall salary rank within each department
+SELECT emp_no, department, salary, RANK() OVER(PARTITION BY department ORDER BY salary DESC) AS overall_salary_rank FROM employees;
+
+-- Select employee details with overall salary rank within each department using ROW_NUMBER
+SELECT emp_no, department, salary, ROW_NUMBER() OVER(PARTITION BY department ORDER BY salary DESC) AS overall_salary_rank FROM employees;
+
+-- Select employee details with dense salary rank within each department
+SELECT emp_no, department, salary, DENSE_RANK() OVER(PARTITION BY department ORDER BY salary DESC) AS dense_salary FROM employees;
+
+-- Select employee details with salary quartiles (both overall and within each department)
+SELECT emp_no, department, salary, NTILE(4) OVER(ORDER BY salary DESC) AS salary_quartile,
+NTILE(4) OVER(PARTITION BY department ORDER BY salary DESC) AS dept_salary_quartile FROM employees;
+
+-- Select employee details with the difference in salary from the previous record within each department
+SELECT emp_no, department, salary, salary - LAG(salary) OVER(PARTITION BY department ORDER BY salary DESC) FROM employees;
